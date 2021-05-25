7d974f30002ccefc861f51b39951d6b3caf66a9d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:49:41 +0530

    Add files via upload

---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void MyDelegate(string msg);                                    // declare a delegate
+    class ClassA
+    {
+        public static void MethodA(string message)
+        {
+            Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+        }
+    }
+
+    class ClassB
+    {
+        public static void MethodB(string message)
+        {
+            Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = new MyDelegate(ClassA.MethodA);               // set target method
+            //yDelegate del = ClassA.MethodA;
+            del("Hello World");                                            // Invoke a delegate
+
+
+            del = ClassB.MethodB;
+            del("Hello World");
+
+            del = (msg) => Console.WriteLine("Called lambda expression: " + msg);           //lambda expression
+            del("Hello World");
+        }
+    }
+}
