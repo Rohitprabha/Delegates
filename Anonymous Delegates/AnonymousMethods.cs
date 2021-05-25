e81ffb4242423e8886770f0c06a697ec40ad405c
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:57:43 +0530

    Add files via upload

---
+++ Anonymous Delegates/AnonymousMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegates_Example_2
+{
+    public delegate string MessageDelegate(string name);
+    class AnonymousMethods
+    {
+        //public static string Message(string name)
+        //{
+        //    return "Hai " + name;
+        //}
+        static void Main(string[] args)
+        {
+            //MessageDelegate obj = new MessageDelegate(Message);
+
+            MessageDelegate obj = delegate (string name)                //AnonymousMethod 
+            {
+                return "Hai " + name;
+            };
+            string str = obj.Invoke("Rohit");
+            Console.WriteLine(str);
+        }
+    }
+}
