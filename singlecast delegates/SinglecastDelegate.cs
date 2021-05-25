7bc6ff98829875763de31c64024955417bcc117a
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:51:40 +0530

    Add files via upload

---
+++ singlecast delegates/SinglecastDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singlecast_Delegate
+{
+    public delegate void AddDelegate(int x, int y);             //Defining a delegate
+    public delegate string DisplayDelegate(string str);
+    class SinglecastDelegate
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static string Display(string name)
+        {
+            return "Hello " + name;
+        }
+        static void Main(string[] args)
+        {
+            SinglecastDelegate s = new SinglecastDelegate();
+            AddDelegate add = new AddDelegate(s.Add);           //Instantiating the Singlecast delegate
+            add(10, 20);
+            add.Invoke(10, 20);                                 //Invoke a delegate
+            DisplayDelegate dis = new DisplayDelegate(Display);
+            Console.WriteLine(dis("Rohit"));
+        }
+    }
+}
