c408439de31b86e7cb422ae22eb831d7aa5a4b1d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-25 19:54:01 +0530

    Add files via upload

---
+++ multicast delegates/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Multiplecast_Delegates
+{
+    public delegate void RectangleDelegate(double Width, double Height);
+    class Rectangle
+    {
+        public void GetArea(double Width, double Height)
+        {
+            Console.WriteLine("Area of rectangle: " + Width * Height);
+        }
+        public void GetPerimeter(double Width, double Height)
+        {
+            Console.WriteLine("Perimeter of rectangle: " + 2 * (Width * Height));
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            RectangleDelegate obj = rect.GetArea;
+            obj += rect.GetPerimeter;                                       //binding 
+            obj(12, 20);                                                    // all the methods will execute with same values 
+        }
+    }
+}
+
